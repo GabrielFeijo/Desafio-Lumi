3448cc855d7378faecc7e1afe73ac277762df156
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-05-21 08:02:03 -0300

    feat: code improvement and add Prisma.

--- lumi-back/src/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { GetHello } from './routes/get-hello';
-
-export const routes = async (app: FastifyInstance) => {
-	app.register(GetHello);
-};
--- lumi-back/src/server.ts
+++ lumi-back/src/server.ts
@@ -1,17 +1,22 @@
 import fastify from 'fastify';
-import { routes } from './routes';
 import cors from '@fastify/cors';
 
 export const app = fastify();
 
-app.register(routes, { prefix: '/api/v1' });
 app.register(cors, { origin: true });
 
-app.listen({ port: 3333 }, (err, address) => {
-	if (err) {
+app.get('/', async () => {
+	return { message: 'Hello World!' };
+});
+
+async function main() {
+	try {
+		await app.listen({ port: 3333 });
+		console.log(`Server running on 3333`);
+	} catch (err) {
 		console.error(err);
 		process.exit(1);
 	}
+}
 
-	console.log(`Server running on ${address}`);
-});
+main();
