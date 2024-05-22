d4c595c3cf066985d68c3b848e39789aec557ead
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-05-21 21:52:27 -0300

    feat: add header component

--- lumi-front/src/app/layout.tsx
+++ lumi-front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
+import { Header } from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,7 +25,10 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					{children}
+					<div className='antialiased'>
+						<Header />
+						{children}
+					</div>
 				</ThemeProvider>
 			</body>
 		</html>
