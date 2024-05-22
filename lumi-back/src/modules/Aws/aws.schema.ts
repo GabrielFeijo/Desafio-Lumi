bd66f1ceef40aef68e68d9104277ee113c37c784
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-05-22 07:53:49 -0300

    chore: add param schema

--- lumi-back/src/modules/Aws/aws.schema.ts
+++ lumi-back/src/modules/Aws/aws.schema.ts
@@ -5,9 +5,16 @@ const uploadResponseSchema = z.object({
 	url: z.string().url(),
 });
 
+const paramsSchema = z.object({
+	filename: z.string(),
+});
+
+export type ParamsSchema = z.infer<typeof paramsSchema>;
+
 export const { schemas: awsSchemas, $ref } = buildJsonSchemas(
 	{
 		uploadResponseSchema,
+		paramsSchema,
 	},
 	{
 		$id: 'awsSchemas',
