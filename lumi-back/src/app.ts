750588dc096cf583324fd5c7332cf3caa551b052
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-05-25 15:34:57 -0300

    feat: customize swagger documentation

--- lumi-back/src/app.ts
+++ lumi-back/src/app.ts
@@ -9,23 +9,59 @@ import { awsSchemas } from './modules/Aws/aws.schema';
 import { customerSchemas } from './modules/Customer/customer.schema';
 import { invoiceSchemas } from './modules/Invoice/invoice.schema';
 import { metricsSchemas } from './modules/Metrics/metrics.schema';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi, { FastifySwaggerUiOptions } from '@fastify/swagger-ui';
+import cors from '@fastify/cors';
+import { getFaviconBuffer } from './utils/get-favicon-buffer';
+import { getLogoBuffer } from './utils/get-logo-buffer';
 
 const defaultOptions = {
 	logger: false,
+};
+
+const swaggerOptions = {
+	swagger: {
+		info: {
+			title: 'Desafio Técnico - Gabriel Feijó',
+			description: 'A REST API built with Fastify, Prisma and TypeScript',
+			version: '1.0.0',
+			contact: {
+				name: 'Gabriel da Silva Feijó',
+				url: 'https://www.linkedin.com/in/gabriel-feijo/',
+				email: 'feijo6622@gmail.com',
+			},
+		},
+
+		basePath: '/',
+		schemes: ['http', 'https'],
+		consumes: ['application/json', 'multipart/form-data'],
+		produces: ['application/json'],
+	},
+};
+
+const swaggerUiOptions: FastifySwaggerUiOptions = {
+	routePrefix: 'api/docs',
+	logo: {
+		type: 'image/png',
+		content: Buffer.from(getLogoBuffer(), 'base64'),
+	},
+	theme: {
+		favicon: [
+			{
+				filename: 'favicon.png',
+				rel: 'icon',
+				sizes: '16x16',
+				type: 'image/png',
+				content: Buffer.from(getFaviconBuffer(), 'base64'),
+			},
+		],
+	},
 };
 
 async function buildApp(options: Partial<typeof defaultOptions> = {}) {
 	const app: FastifyInstance = Fastify({
 		...defaultOptions,
 		...options,
-	});
-
-	app.register(fastifyMultipart, {
-		limits: { fileSize: 5 * 1024 * 1024 }, // Limite de 5 MB
-	});
-
-	app.get('/', async () => {
-		return { message: 'Hello World!' };
 	});
 
 	for (const schema of [
@@ -36,6 +72,24 @@ async function buildApp(options: Partial<typeof defaultOptions> = {}) {
 	]) {
 		app.addSchema(schema);
 	}
+
+	app.register(fastifyMultipart, {
+		limits: { fileSize: 5 * 1024 * 1024 }, // Limite de 5 MB
+	});
+
+	app.get('/', async () => {
+		return { message: 'Hello World!' };
+	});
+
+	app.register(cors, { origin: true });
+
+	app.register(fastifySwagger, swaggerOptions);
+	app.register(fastifySwaggerUi, swaggerUiOptions);
+
+	app.ready((err) => {
+		if (err) throw err;
+		app.swagger();
+	});
 
 	app.register(metricRoutes, { prefix: 'api/metrics' });
 	app.register(customerRoutes, { prefix: 'api/customers' });
--- lumi-back/src/server.ts
+++ lumi-back/src/server.ts
@@ -1,46 +1,8 @@
 import 'dotenv/config';
-import cors from '@fastify/cors';
-import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUi from '@fastify/swagger-ui';
 import buildApp from './app';
-
-const swaggerOptions = {
-	swagger: {
-		info: {
-			title: 'Desafio Técnico - Gabriel Feijó',
-			description: 'A REST API built with Fastify, Prisma and TypeScript',
-			version: '1.0.0',
-			contact: {
-				name: 'Gabriel da Silva Feijó',
-				url: 'https://www.linkedin.com/in/gabriel-feijo/',
-				email: 'feijo6622@gmail.com',
-			},
-		},
-
-		basePath: '/',
-		schemes: ['http', 'https'],
-		consumes: ['application/json', 'multipart/form-data'],
-		produces: ['application/json'],
-	},
-};
-
-const swaggerUiOptions = {
-	routePrefix: 'api/docs',
-	exposeRoute: true,
-};
 
 export async function main() {
 	const app = await buildApp();
-
-	app.register(fastifySwagger, swaggerOptions);
-	app.register(fastifySwaggerUi, swaggerUiOptions);
-
-	app.ready((err) => {
-		if (err) throw err;
-		app.swagger();
-	});
-
-	app.register(cors, { origin: true });
 
 	try {
 		const port = Number(process.env.PORT) || 3333;
